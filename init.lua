b187813a03bbb27c9d4546ebb87ca76a0250802e
Louis Moulin <louis.moulin@outlook.fr>
2025-08-03 22:53:05 +0200

    min; need more lsp config

--- init.lua
+++ init.lua
@@ -1,1 +1,38 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = "yes"
+vim.o.termguicolors = true
+vim.o.wrap = false
+vim.o.tabstop = 2
+vim.o.swapfile = false
+vim.g.mapleader = " "
+vim.o.winborder = "rounded"
+vim.o.clipboard = "unnamedplus"
 
+vim.pack.add({
+	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+	end,
+})
+vim.cmd("set completeopt+=noselect")
+
+require "mini.pick".setup()
+require "nvim-treesitter.configs".setup({
+	highlight = { enable = true }
+	auto_install = true,
+})
+vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
+vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+vim.lsp.enable({ "lua_ls" })
+
+vim.cmd(":hi statusline guibg=NONE")
