d9be1b332a68928ff822e18a842630d89bfd01b6
Louis Moulin <louis.moulin@outlook.fr>
2025-08-03 23:19:35 +0200

    wip

--- init.lua
+++ init.lua
@@ -11,6 +11,11 @@ vim.o.clipboard = "unnamedplus"
 
 vim.pack.add({
 	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/echasnovski/mini.pairs" },
+	{ src = "https://github.com/echasnovski/mini.completion" },
+	{ src = "https://github.com/echasnovski/mini.notify" },
+	{ src = "https://github.com/echasnovski/mini.icons" },
+	{ src = "https://github.com/echasnovski/mini.extra" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 })
@@ -26,13 +31,23 @@ vim.api.nvim_create_autocmd('LspAttach', {
 vim.cmd("set completeopt+=noselect")
 
 require "mini.pick".setup()
+require "mini.pairs".setup()
+require "mini.completion".setup()
+require "mini.notify".setup()
+require "mini.icons".setup()
+require "mini.extra".setup()
+
 require "nvim-treesitter.configs".setup({
 	highlight = { enable = true },
 	auto_install = true,
 })
-vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
-vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+
+vim.keymap.set('n', '<leader>pf', ":Pick files<CR>")
+vim.keymap.set('n', '<leader>ph', ":Pick help<CR>")
+vim.keymap.set('n', '<leader>pd', ":Pick diagnostic<CR>")
+
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
+
 vim.lsp.enable({ "lua_ls", "rust_analyzer"})
 
 vim.cmd(":hi statusline guibg=NONE")
