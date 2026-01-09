54f99d46944f747548d3b78e77ca87e51944d422
Louis Moulin <louis.moulin@outlook.fr>
2026-01-09 23:08:11 +0100

    wip

--- init.lua
+++ init.lua
@@ -1,5 +1,8 @@
 vim.loader.enable()
 
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
 vim.opt.winborder = 'rounded'
 vim.opt.cursorcolumn = false
 vim.opt.ignorecase = true
@@ -18,40 +21,41 @@ vim.opt.clipboard = 'unnamedplus'
 -- Enable auto reloading when content change
 vim.o.autoread = true
 vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter' }, {
-	pattern = '*',
-	command = 'checktime',
+  pattern = '*',
+  command = 'checktime',
 })
 
 vim.pack.add {
-	-- Collection of a lot of stuff
-	{ src = 'https://github.com/echasnovski/mini.nvim' },
-	-- LSP completion
-	{ src = 'https://github.com/Saghen/blink.cmp' },
-	-- Install LSP
-	{ src = 'https://github.com/mason-org/mason.nvim' },
-	-- File Explorer
-	{ src = 'https://github.com/stevearc/oil.nvim' },
-	-- Theme
-	{ src = 'https://github.com/folke/tokyonight.nvim' },
-	-- gitsigns
-	{ src = 'https://github.com/lewis6991/gitsigns.nvim' },
-	-- Format plugin
-	{ src = 'https://github.com/stevearc/conform.nvim' },
-	-- fuzzy finder
-	{ src = 'https://github.com/nvim-telescope/telescope.nvim' },
-	{ src = 'https://github.com/nvim-lua/plenary.nvim' },
-	-- Syntax hightligtin
-	{ src = 'https://github.com/nvim-treesitter/nvim-treesitter',          version = 'main' },
+  -- Collection of a lot of stuff
+  { src = 'https://github.com/echasnovski/mini.nvim' },
+  -- LSP completion
+  { src = 'https://github.com/Saghen/blink.cmp' },
+  -- Install LSP
+  { src = 'https://github.com/mason-org/mason.nvim' },
+  -- File Explorer
+  { src = 'https://github.com/stevearc/oil.nvim' },
+  -- Theme
+  { src = 'https://github.com/folke/tokyonight.nvim' },
+  -- gitsigns
+  { src = 'https://github.com/lewis6991/gitsigns.nvim' },
+  -- Format plugin
+  { src = 'https://github.com/stevearc/conform.nvim' },
+  -- fuzzy finder
+  { src = 'https://github.com/nvim-telescope/telescope.nvim' },
+  { src = 'https://github.com/nvim-lua/plenary.nvim' },
+  -- Syntax highlighting
+  {
+    src = 'https://github.com/nvim-treesitter/nvim-treesitter',
+    version = 'main',
+  },
 
-	{ src = 'https://github.com/folke/todo-comments.nvim' },
-	{ src = 'https://github.com/folke/tokyonight.nvim' },
+  { src = 'https://github.com/folke/todo-comments.nvim' },
 
-	-- Markdown preview
-	{ src = 'https://github.com/MeanderingProgrammer/render-markdown.nvim' },
+  -- Markdown preview
+  { src = 'https://github.com/MeanderingProgrammer/render-markdown.nvim' },
 
-	-- Crates Version
-	{ src = 'https://github.com/saecki/crates.nvim' },
-
+  -- Crates Version
+  { src = 'https://github.com/saecki/crates.nvim' },
 }
 
 require('todo-comments').setup()
@@ -74,54 +78,80 @@ vim.keymap.set('n', '<leader>sd', tel_builtin.diagnostics, { desc = 'Telescope b
 vim.keymap.set('n', '<leader>sr', tel_builtin.resume, { desc = 'Telescope resume search' })
 
 require('oil').setup {
-	columns = {
-		'icon',
-		'size',
-	},
-	watch_for_changes = true,
-	view_options = {
-		show_hidden = true,
-	},
+  columns = {
+    'icon',
+    'size',
+  },
+  watch_for_changes = true,
+  view_options = {
+    show_hidden = true,
+  },
 }
 vim.keymap.set('n', '<leader>e', ':Oil<CR>')
 
 require('blink.cmp').setup {
-	fuzzy = { implementation = 'lua' },
+  fuzzy = { implementation = 'lua' },
 }
 
 require('mason').setup()
-vim.lsp.enable { 'lua_ls', 'rust_analyzer', 'vtsls', 'vue_ls', 'terraformls', 'tflint', 'ruff', 'jsonls', 'ty', 'nil_ls' }
+vim.lsp.enable {
+  'lua_ls',
+  'rust_analyzer',
+  --INFO: Vue TS
+  'vtsls',
+  'vue_ls',
+  'jsonls',
+  --INFO: Terraform/Terragrunt
+  'terraformls',
+  'tflint',
+  --INFO: Python
+  'ruff',
+  'ty',
+  'nil_ls',
+  'helm_ls',
+  'yamlls',
+  --INFO: Go
+  'gopls',
+  'golangci_lint_ls',
+}
 vim.lsp.inlay_hint.enable(true)
 
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Show diagnostics' })
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code action' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setqflist, { desc = 'Diagnostics to quickfix' })
+vim.keymap.set('n', '<leader>cF', function()
+  vim.fn.setreg('+', vim.fn.expand '%:p')
+  print('Copied: ' .. vim.fn.expand '%:p')
+end, { desc = 'Copy absolute file path' })
+
 require('conform').setup {
-	formatters_by_ft = {
-		lua = { 'stylua', lsp_format = 'fallback' },
-		rust = { 'rustfmt', lsp_format = 'fallback' },
-		python = {
-			'ruff_fix',
-			'ruff_format',
-			'ruff_organize_imports',
-		},
-	},
+  formatters_by_ft = {
+    lua = { 'stylua', lsp_format = 'fallback' },
+    rust = { 'rustfmt', lsp_format = 'fallback' },
+    python = {
+      'ruff_fix',
+      'ruff_format',
+      'ruff_organize_imports',
+    },
+    go = { 'goimports', 'gofmt' },
+  },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_format = 'fallback',
+  },
 }
-vim.api.nvim_create_autocmd('BufWritePre', {
-	pattern = '*',
-	callback = function(args)
-		require('conform').format { bufnr = args.buf }
-	end,
-})
-vim.keymap.set('n', '<leader>f', function()
-	require('conform').format()
-end)
 
 require('gitsigns').setup {}
 
+require('nvim-treesitter').setup {
+  install_dir = vim.fn.stdpath 'data' .. '/site',
+}
 vim.api.nvim_create_autocmd('FileType', {
-	pattern = { 'python' },
-	callback = function()
-		vim.treesitter.start()
-	end,
+  pattern = { 'python', 'rust', 'go', 'lua', 'javascript', 'typescript', 'vue', 'json', 'yaml', 'toml', 'markdown' },
+  callback = function()
+    vim.treesitter.start()
+  end,
 })
 
 require('render-markdown').setup {}
